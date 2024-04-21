3636e307223e8a3aa026256acda2a1891cc9057b
sidhart25 <navneetnishchal1420@gmail.com>
2024-04-21 08:05:36 +0530

    tree distance problem solved

---
+++ trees/tree-distance.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define mod LLONG_MAX
+ 
+ 
+ 
+ll power(ll x, ll y)
+{
+    ll temp;
+    if (y == 0){
+        ll ans=1;
+        return ans;
+ 
+    }
+       
+    temp = power(x, y / 2);
+    if (y % 2 == 0)
+        return ((temp%mod) * (temp%mod))%mod;
+    else{
+        ll p1=((x%mod)*(temp%mod))%mod;
+        ll p2=((p1%mod)*(temp%mod))%mod;
+        return p2;
+    }
+        
+}
+ 
+ 
+ 
+ 
+void dfs1(vector<vector<int>>&tree,int curr,int p,vector<int>&dp){
+    dp[curr]=0;
+    int mmax=0;
+    for(int i=0;i<tree[curr].size();i++){
+        int child=tree[curr][i];
+        if(child != p){
+            dfs1(tree,child,curr,dp);
+            mmax=max(mmax,dp[child]+1);
+        }
+    }
+    dp[curr]=mmax;
+}
+
+void dfs(vector<vector<int>>&tree,int curr,int p,vector<int>&in,vector<int>&out){
+    int max_dis_1=-1;
+    int max_dis_2=-1;
+    for(int i=0;i<tree[curr].size();i++){
+        int child=tree[curr][i];
+        if(child != p){
+            if(max_dis_1<in[child]){
+                max_dis_2=max_dis_1;
+                max_dis_1=in[child];
+            }else if(max_dis_2<in[child]){
+                max_dis_2=in[child];
+            }
+
+        }
+    }
+
+    for(int i=0;i<tree[curr].size();i++){
+        int child=tree[curr][i];
+        if(child != p){
+            int max_dis=max_dis_1;
+            if(max_dis==in[child]){
+                max_dis=max_dis_2;
+            }
+            out[child]=1+max(out[curr],1+max_dis);
+            dfs(tree,child,curr,in,out);
+        }
+    }
+}
+ 
+ 
+void solve(){
+   int n;
+   cin>>n;
+   vector<int>dept(n+1,0);
+   vector<vector<int>>tree(n+1);
+   vector<int>parent(n+1,1);
+   for(int i=1;i<n;i++){
+     int u,v;
+     cin>>u>>v;
+     tree[u].push_back(v);
+     tree[v].push_back(u);
+   }
+   int node=1;
+   vector<int>in(n+1,0);
+   dfs1(tree,node,node,in);
+   vector<int>out(n+1,0);
+   dfs(tree,node,0,in,out);
+   for(int i=1;i<=n;i++){
+    cout<<max(in[i],out[i])<<endl;
+   }
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    ll t=1;
+    
+    //cin>>t;
+    while(t--){
+        solve();
+     }
+ 
+    return 0;
+}
