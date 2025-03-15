c75fd9ef13894e6e5be1495bbf0ac688c47f2f18
sidhart25 <navneetnishchal1420@gmail.com>
2025-03-15 08:48:21 +0530

    edit distance problem solved using dp

--- dp/editDistance.cpp
+++ dp/editDistance.cpp
@@ -118,11 +118,31 @@ void solutionOne(){
     vector<vector<int>>dp(m+1,vector<int>(n+1,INT_MAX));
     dp[0][0]=0;
 
-    for(int i=0;i<=m;i++){
-        for(int j=0;j<=n;j++){
+    for(int i=1;i<=n;i++){
+        dp[0][i]=i;
+    }
+    for(int i=1;i<=m;i++){
+        dp[i][0]=i;
+    }
+
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
+            if(first[j-1]==second[i-1]){
+                dp[i][j]=dp[i-1][j-1];
+            }else{
+                // insertion on string first
+                dp[i][j]=min(dp[i][j],dp[i-1][j]+1);
+                // deletion of string first
+                dp[i][j]=min(dp[i][j],dp[i][j-1]+1);
+
+                // replace char of string first
+                dp[i][j]=min(dp[i][j],dp[i-1][j-1]+1);
+            }
            
             }
         }
+
+        cout<<dp[m][n]<<endl;
     }
 
 
@@ -136,7 +156,8 @@ int main(){
 	cin.tie(0);
 
     // solutionTwo();
-    solutionThree();
+    // solutionThree();
+    solutionOne();
 
     return 0;
 }
