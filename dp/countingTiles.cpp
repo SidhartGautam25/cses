77ad11bfafd0678ae9b7f0feb40ef9828cad5428
sidhart25 <navneetnishchal1420@gmail.com>
2025-03-23 23:52:16 +0530

    solving counting tiles problem

---
+++ dp/countingTiles.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define mod 1000000007
+
+void helperOne(int n,int m,int col,int mask){
+    if(col==m){
+        return (mask==0);
+    }
+
+    
+
+}
+
+void solutionOne(){
+    int n,m;
+    cin>>n>>m;
+    helperOne(n,m,0,0);
+
+    
+
+}
+   
+   
+
+   
+
+
+
+int main(){
+    ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+    // solutionTwo();
+    // solutionThree();
+    solutionOne();
+
+    return 0;
+}
