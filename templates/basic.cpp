71e19ed0253ea33290c0eeda9b70b96f7a3103f1
sidhart25 <navneetnishchal1420@gmail.com>
2025-03-26 13:08:45 +0530

    basic template is ready my friend

---
+++ templates/basic.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define mod 1000000007
+
+
+void read_iv(vector<int>&arr){
+    int n=arr.size();
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void print_iv(vector<int>&arr){
+    int n=arr.size();
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void read_i(int& x){
+    cin>>x;
+}
+
+
+
+
+void solutionOne() {
+
+  
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    solutionOne();
+
+    return 0;
+}
